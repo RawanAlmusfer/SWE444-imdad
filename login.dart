3643a6e354768396d2e848f123b9e93d39175a56
Raneem AlRashoud <59771760+RaneemAlrashoud@users.noreply.github.com>
2021-09-16 06:39:59 +0300

    Create login.dart

---
+++ login.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:login_demo/auth.dart';
+import 'package:login_demo/auth_provider.dart';
+
+class EmailFieldValidator {
+  static String validate(String value) {
+    return value.isEmpty ? 'Email can\'t be empty' : null;
+  }
+}
+
+class PasswordFieldValidator {
+  static String validate(String value) {
+    return value.isEmpty ? 'Password can\'t be empty' : null;
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({this.onSignedIn});
+  final VoidCallback onSignedIn;
+
+  @override
+  State<StatefulWidget> createState() => _LoginPageState();
+}
+
+enum FormType {
+  login,
+  register,
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+
+  bool validateAndSave() {
+    final FormState form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  Future<void> validateAndSubmit() async {
+    if (validateAndSave()) {
+      try {
+        final BaseAuth auth = AuthProvider.of(context).auth;
+        if (_formType == FormType.login) {
+          final String userId = await auth.signInWithEmailAndPassword(_email, _password);
+          print('Signed in: $userId');
+        } else {
+          final String userId = await auth.createUserWithEmailAndPassword(_email, _password);
+          print('Registered user: $userId');
+        }
+        widget.onSignedIn();
+      } catch (e) {
+        print('Error: $e');
+      }
+    }
+  }
+
+  void moveToRegister() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter login demo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: buildInputs() + buildSubmitButtons(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildInputs() {
+    return <Widget>[
+      TextFormField(
+        key: Key('email'),
+        decoration: InputDecoration(labelText: 'Email'),
+        validator: EmailFieldValidator.validate,
+        onSaved: (String value) => _email = value,
+      ),
+      TextFormField(
+        key: Key('password'),
+        decoration: InputDecoration(labelText: 'Password'),
+        obscureText: true,
+        validator: PasswordFieldValidator.validate,
+        onSaved: (String value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons() {
+    if (_formType == FormType.login) {
+      return <Widget>[
+        RaisedButton(
+          key: Key('signIn'),
+          child: Text('Login', style: TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        FlatButton(
+          child: Text('Create an account', style: TextStyle(fontSize: 20.0)),
+          onPressed: moveToRegister,
+        ),
+      ];
+    } else {
+      return <Widget>[
+        RaisedButton(
+          child: Text('Create an account', style: TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        FlatButton(
+          child: Text('Have an account? Login', style: TextStyle(fontSize: 20.0)),
+          onPressed: moveToLogin,
+        ),
+      ];
+    }
+  }
+}
