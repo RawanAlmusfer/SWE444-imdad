87843cc8285f65251160a2ec9e7092c8ce941d3c
Rawan Mohammed <rawanmohaamed213@gmail.com>
2021-09-20 10:09:44 +0300

    edit main

--- swe444/lib/main.dart
+++ swe444/lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'package:swe444/post_request.dart';
+import 'package:swe444/post_request.dart';
 import 'package:swe444/home.dart';
-import 'package:swe444/xd_post_request.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(Trial());
+  runApp(MyApp());
 }
 
-class Trial extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home:
-      PostRequest(),
-    );
-  }
-}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
