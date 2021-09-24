8d57eb10238048d60a497c44f7ef40075775e277
Raneem AlRashoud <59771760+RaneemAlrashoud@users.noreply.github.com>
2021-09-24 21:07:58 +0300

    Add files via upload

---
+++ Method#1/decisions_tree.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class DecisionsTree extends StatefulWidget {
+  @override
+  _DecisionsTreeState createState() => _DecisionsTreeState();
+}
+
+class _DecisionsTreeState extends State<DecisionsTree> {
+  //const user = FirebaseAuth.instance.currentUser;
+  User? user = FirebaseAuth.instance.currentUser;
+
+
+  @override
+  // void initState(){
+  //   super.initState();
+  //   onRefresh(FirebaseAuth.instance.currentUser);
+  //
+  //
+  // }
+
+
+
+
+
+  onRefresh(userCred) {
+    setState(() {
+      User user = userCred;
+    });}
+
+
+  @override
+  Widget build(BuildContext context) {
+    if (user == null) {
+    return LoginPage(
+      onSignIn: (userCred) => onRefresh(userCred),
+    );
+  }
+
+     return HomePage();
+
+    /*if (user) {
+      // user is signed in, show user data
+      return HomePage();
+    }
+    else {
+      // user is signed out, show sign-in form
+
+
+      return LoginPage(
+        onSignIn: (userCred) => onRefresh(userCred),
+      );*/
+
+
+  }
+}
---
+++ Method#1/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+//it should be as profile page
+
+class HomePage extends StatelessWidget {
+
+  //final Function(User) onSignOut;
+
+  //HomePage({required this.onSignOut});
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  /*Future <void> logout()async {
+    await FirebaseAuth.instance.signOut();
+    onSignOut();*/
+
+
+  Future <void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+//signOut();
+// onSignOut();
+
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text("الرئيسية")),
+    body: Center(
+      child: Container(
+          child: RaisedButton(child: Text(  "تسجيل الخروج"),
+            onPressed: () {
+              signOut();
+            },
+
+          )
+      ),
+    ),
+  );
+} }
