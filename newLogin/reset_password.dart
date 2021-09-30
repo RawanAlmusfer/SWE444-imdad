21acb2f38dbb11404b5a1b1827fee2416d1e3906
Raneem AlRashoud <59771760+RaneemAlrashoud@users.noreply.github.com>
2021-09-30 12:37:20 +0300

    Add files via upload

---
+++ newLogin/reset_password.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/painting.dart';
+import 'login_page.dart';
+import 'decisions_tree.dart';
+import 'users_screen.dart';
+
+class ResetPasswordScreen extends StatefulWidget {
+  @override
+  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+
+  static const kYellow = const Color(0xdeedd03c);
+  TextEditingController _controllerEmail = TextEditingController();
+  final auth = FirebaseAuth.instance;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffededed),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('إعادة ضبط كلمة السر',    textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color(0xff334856),
+            fontWeight: FontWeight.w700,
+            fontFamily: 'Tajawal',
+            fontSize: 24,
+          ),),
+
+        bottomOpacity: 30,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(50),
+          ),
+        ),
+
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => DecisionsTree()));
+              },
+              child: Icon(Icons.arrow_forward_ios),
+            ),
+          )
+        ],
+        iconTheme: IconThemeData(
+          // Padding(padding: EdgeInsets.only(right: 20.0)),
+
+          color: Color(0xff334856), //change your color here.
+        ),
+        backgroundColor: const Color(0xdeedd03c),
+
+
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0,top: 70),
+                      child: Container(
+                          height: 160,
+                          width: 160,
+                          child: Image.asset('images/logo.png')),
+                    ),
+                    Padding(
+                      padding:  EdgeInsets.only(left: 0.0, top: 115),
+                      child: Text(
+                        "مرحباً بك في",
+                        style: TextStyle(
+                          color: Color(0xff334856),
+                          fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Tajawal',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.05,
+                ),
+                new Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: new TextFormField(
+                    showCursor: true,
+                    cursorColor: const Color(0xdeedd03c),
+                    textAlign: TextAlign.right,
+                    controller: _controllerEmail,
+                    decoration: new InputDecoration(
+                      focusedBorder:  OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        // width: 0.0 produces a thin "hairline" border
+                        borderSide: const BorderSide(
+                          color: kYellow,
+                        ),
+                      ),
+                      prefixIcon: Icon(Icons.email_outlined,color: const Color(0xff334856),),
+                      prefixStyle: TextStyle(
+                          fontSize: 18, color: const Color(0xff334856)),
+                      hoverColor: const Color(0xff334856),
+                      hintText: 'أدخل البريد الالكتروني',
+                      labelText: 'البريد الالكتروني',
+                      hintStyle: TextStyle(
+                          fontSize: 14,
+                          color: const Color(0xff334856),
+                          fontFamily: 'Tajawal'),
+                      labelStyle: TextStyle(
+                          fontSize: 18,
+                          color: const Color(0xff334856),
+                          fontFamily: 'Tajawal'),
+                      alignLabelWithHint: true,
+                      border: OutlineInputBorder(),
+                    ),
+                    autocorrect: false,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.03,
+                ),
+
+                SizedBox(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.02,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                    ),
+
+
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.04,
+                ),
+                ElevatedButton(
+                  onPressed: () async{
+                  await  auth.sendPasswordResetEmail(email: _controllerEmail.text)
+                        .then((value) =>
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DecisionsTree())));
+                  },
+                  child: Text(
+                    'إعادة ضبط كلمة السر',
+                    style: TextStyle(color: Color(0xff334856),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,),
+                  ),
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor:
+                    MaterialStateProperty.all(Color(0xdeedd03c)),
+                    minimumSize: MaterialStateProperty.all(Size(300, 64)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(500),
+                      ),
+                    ),
+
+                  ),
+                )
+
+                // RaisedButton(
+                //     onPressed: () {
+                //       isLogin ? login() : createUser();
+                //     },
+                //     child: Text(isLogin ? "تسجيل الدخول" : "التسجيل")),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ newLogin/users_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'singup_login_screen.dart';
+
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  static const kYellow = const Color(0xdeedd03c);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: const Color(0xdeedd03c),
+      ),
+      body: new Container(
+
+        padding: new EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(),
+            child: Column(
+              children: [
+                Image.asset('images/logo.png'),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Text(
+                  "مرحباً بك",
+                  style: TextStyle(
+                    color: Color(0xff334856),
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                    ),
+
+                  ],
+                ),
+
+                ElevatedButton(
+
+                  onPressed: () {  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => SignupLoginScreen()));
+                  },
+
+                  child: Text(
+                    'متطوع',
+                    style: TextStyle(   color: Color(0xff334856), fontSize: 20,fontWeight: FontWeight.bold,),
+                  ),
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xdeedd03c)),
+                    minimumSize: MaterialStateProperty.all(Size(300, 64)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(500),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => SignupLoginScreen()));
+
+                  },
+                  child: Text(
+                    'مالك مسجد',
+                    style: TextStyle(   color: Color(0xff334856), fontSize: 20,fontWeight: FontWeight.bold,),
+                  ),
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor:
+                    MaterialStateProperty.all(Color(0xdeedd03c)),
+                    minimumSize: MaterialStateProperty.all(Size(300, 64)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(500),
+                      ),
+                    ),
+
+                  ),
+                )
+
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
