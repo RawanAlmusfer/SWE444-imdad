71fa5a48de22170484078131b15d55ec86cf7469
Rawan Mohammed <rawanmohaamed213@gmail.com>
2021-10-08 20:11:47 +0300

    update url launcher

--- swe444/lib/Functions/home_screen/v_feed.dart
+++ swe444/lib/Functions/home_screen/v_feed.dart
@@ -68,6 +68,7 @@ class vFeed extends State<v_feed> {
   }
 
   Widget buildCards(BuildContext context, DocumentSnapshot document) {
+    FeedViewModel feedVM= FeedViewModel();
     //print("location:" + document['location'].toString());
     return Container(
       padding: const EdgeInsets.only(top: 10.0, left: 13, right: 13),
@@ -234,8 +235,8 @@ class vFeed extends State<v_feed> {
                   Spacer(),
                   IconButton(
                     icon: Icon(Icons.location_on, color: Color(0xdeedd03c)),
-                    onPressed: () {
-                      _lunchURL(document['mosque_location'].toString());
+                    onPressed: () async {
+                      await feedVM.lunchURL(document['mosque_location'].toString());
                     },
                   ),
                 ]),
@@ -245,14 +246,6 @@ class vFeed extends State<v_feed> {
         ),
       ),
     );
-  }
-}
-
-Future<void> _lunchURL(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw "Could not lunch the url";
   }
 }
 
