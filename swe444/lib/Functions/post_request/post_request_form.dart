4fec5b96673b0382092308368e04c8f1bf1034ec
Rawan Mohammed <rawanmohaamed213@gmail.com>
2021-10-16 23:25:56 +0300

    Update post_request_form.dart

--- swe444/lib/Functions/post_request/post_request_form.dart
+++ swe444/lib/Functions/post_request/post_request_form.dart
@@ -26,9 +26,10 @@ class _AddRequestFormState extends State<PostRequestForm> {
   int charLength= 0;
   TextEditingController _amount = TextEditingController();
   TextEditingController title = TextEditingController();
-  final TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController itemD = TextEditingController();
+  TextEditingController itemsAmount = TextEditingController();
   DateTime time = DateTime.now();
-  final List<String> items = <String>['مبلغ'];
+  final List<String> items = <String>['مبلغ', 'موارد'];
   TextEditingController description = TextEditingController();
 
   Widget _buildType() {
@@ -83,6 +84,7 @@ class _AddRequestFormState extends State<PostRequestForm> {
       value: type,
       items: items.map<DropdownMenuItem<String>>((dropdownMenuItem) {
         return DropdownMenuItem(
+          key: UniqueKey(),
           value: dropdownMenuItem,
           child: SizedBox(
             width: 200.w,
@@ -131,7 +133,7 @@ class _AddRequestFormState extends State<PostRequestForm> {
         if (value.length > 30) return "لا يمكن ان يزيد عن 30 حرف ";
       },
       controller: title,
-      onSaved: (_val) {
+      onFieldSubmitted: (_val) {
         if (_val != null) title.text = _val;
       },
 
@@ -178,7 +180,7 @@ class _AddRequestFormState extends State<PostRequestForm> {
     );
   }
 
-  Widget _buildDetails() {
+  Widget _buildDetailsFunds() {
     double _value;
     return TextFormField(
       validator: (value) {
@@ -219,9 +221,129 @@ class _AddRequestFormState extends State<PostRequestForm> {
             fontSize: 19,
             color: const Color(0xffcbcbcc),
             fontFamily: 'Tajawal'),
+        labelStyle: const TextStyle(
+            fontSize: 15,
+            color: Color(0xff334856),
+            fontFamily: 'Tajawal'),
+      ),
+      inputFormatters: [
+        LengthLimitingTextInputFormatter(30),
+        FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+      ],
+      controller: _amount,
+      keyboardType: TextInputType.number,
+      onFieldSubmitted: (_val) {
+        if (_val != null) {
+          _amount.text = _val;
+        }
+      }, // onsaved
+    );
+  }
+
+  Widget _buildDetailsItems() {
+    return TextFormField(
+      maxLines: 1,
+      maxLength: 30,
+
+      validator: (value) {
+        if (value == null || value.isEmpty || value.trim().isEmpty)
+          return "مطلوب";
+        if (!RegExp(r"^[\p{L} ,.'-]*$",
+            caseSensitive: false, unicode: true, dotAll: true)
+            .hasMatch(value)) return "يجب أن يحتوي على أحرف فقط";
+        if (value.length > 30) return "لا يمكن ان يزيد عن 30 حرف ";
+      },
+      controller: itemD,
+      onFieldSubmitted: (_val) {
+        if (_val != null) itemD.text = _val;
+      },
+      onChanged: (value) {
+        setState( () {
+          charLength = value.length;
+        },
+        );
+      },
+
+      showCursor: true,
+      cursorColor: const Color(0xdeedd03c),
+      style: TextStyle(fontSize: 18, color: const Color(0xff334856)),
+      textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        // counterText: '${_enteredText.length.toString()}character(s)',
+        contentPadding: EdgeInsets.only(right: 20, top: 15 ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        focusedBorder: OutlineInputBorder(
+          // width: 0.0 produces a thin "hairline" border
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: const Color(0xdeedd03c),
+          ),
+        ),
+        prefixStyle: TextStyle(fontSize: 15, color: const Color(0xff334856)),
+        hoverColor: const Color(0xff334856),
+        hintText: 'أدخل اسم المورد المطلوب',
+        labelText: 'المورد *',
+        hintStyle: TextStyle(
+            fontSize: 13,
+            color: const Color(0xffcbcbcc),
+            fontFamily: 'Tajawal'),
         labelStyle: TextStyle(
             fontSize: 15,
             color: const Color(0xff334856),
+            fontFamily: 'Tajawal'),
+        alignLabelWithHint: true,
+        //border: OutlineInputBorder(),
+        // hoverColor: const Color(0xff334856),
+      ),
+    );
+  }
+
+  Widget _buildDetailsItemsAmount() {
+    double _value;
+    return TextFormField(
+      validator: (value) {
+        if (value == null ||
+            value.isEmpty ||
+            value.trim().isEmpty)
+          return "مطلوب";
+        else {
+          _value = double.parse(value);
+          if (_value > 200) return "الآقصى= 200";
+          if (_value < 5) return "الآدنى= 5";
+        }
+      },
+      textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.only(right: 20, top: 15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        focusedBorder: OutlineInputBorder(
+          // width: 0.0 produces a thin "hairline" border
+
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: const Color(0xdeedd03c),
+          ),
+        ),
+        prefixStyle: TextStyle(
+            fontSize: 15,
+            color: const Color(0xff334856),
+            fontFamily: 'Tajawal'),
+        hoverColor: const Color(0xff334856),
+        alignLabelWithHint: true,
+        //border: OutlineInputBorder(),
+        hintText: '00',
+        labelText: 'الكمية المطلوبة *',
+        hintStyle: TextStyle(
+            fontSize: 19,
+            color: const Color(0xffcbcbcc),
+            fontFamily: 'Tajawal'),
+        labelStyle: const TextStyle(
+            fontSize: 15,
+            color: Color(0xff334856),
             fontFamily: 'Tajawal'),
       ),
       inputFormatters: [
@@ -390,6 +512,8 @@ class _AddRequestFormState extends State<PostRequestForm> {
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.005,
             ),
+
+            if (type != null)
             Container(
               width: portrait == true ? 250.w : 300.w,
               child: Directionality(
@@ -407,16 +531,18 @@ class _AddRequestFormState extends State<PostRequestForm> {
 
               ),
             ),
-            SizedBox(
+            if (type != null)
+              SizedBox(
               height: MediaQuery.of(context).size.height * 0.015,
             ),
-            Container(
+            if (type == "مبلغ")
+              Container(
               width: portrait == true ? 250.w : 300.w,
               child: Stack(
                 children: [
                   Directionality(
                     textDirection: TextDirection.rtl,
-                    child: _buildDetails(),
+                    child: _buildDetailsFunds(),
             ),Directionality(
                     textDirection: TextDirection.ltr,
                     child: Padding(
@@ -433,10 +559,31 @@ class _AddRequestFormState extends State<PostRequestForm> {
                     ),
                   )
                 ],
-              ) ), // confirm container
+              ) ),
+            if (type == "موارد")
+              Container(
+                  width: portrait == true ? 250.w : 300.w,
+                  child: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: _buildDetailsItems(),
+                  ) ),
+            if (type == "موارد")
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.015,
+              ),
+            if (type == "موارد")
+              Container(
+                  width: portrait == true ? 250.w : 300.w,
+                  child: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: _buildDetailsItemsAmount(),
+                  ) ),
+            if (type != null) // funds container
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.025,
             ),
+
+
             Container(
               width: portrait == true ? 250.w : 300.w,
               child: Directionality(
