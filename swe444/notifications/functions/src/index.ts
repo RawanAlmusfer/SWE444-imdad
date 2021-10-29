cf7200991632ced12c702854426db3cb56ab4223
Deema AlMutairi <dema77.da@gmail.com>
2021-10-30 00:51:35 +0300

    Update index.ts

--- swe444/notifications/functions/src/index.ts
+++ swe444/notifications/functions/src/index.ts
@@ -2,30 +2,32 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-export const notifyMosqueManager = functions.firestore.document("requests/{requestID}").onCreate((snapshot, context) => {
-  const data = snapshot.data();
-  admin.firestore().collection("tokens").get().then(async (snapshots) => {
-    const tokens = [];
-    if (snapshots.empty) {
-      console.log("No Device");
-    } else {
-      for (const token of snapshots.docs) {
-        tokens.push(token.data().token);
+export const notifyMosqueManager = functions.firestore.document("requests/{requestID}").onUpdate((snapshot, context) => {
+  const data = snapshot.after.data();
+  if (data.amount == data.donated) {
+    admin.firestore().collection("tokens").get().then(async (snapshots) => {
+      const tokens = [];
+      if (snapshots.empty) {
+        console.log("No Device");
+      } else {
+        for (const token of snapshots.docs) {
+          tokens.push(token.data().token);
+        }
+        const payloadData = {
+          title: "تم اكتمال المبلغ",
+          message: "لقد تم اكتمال طلبك:" + " " + data.title,
+        };
+        const payload = {
+          data: payloadData,
+        };
+        return await admin.messaging().sendToDevice(tokens, payload).then((response) => {
+          console.log("Pushed All Notifications");
+        });
       }
-      const payloadData = {
-        title: "تم اكتمال المبلغ",
-        message: "لقد تم اكتمال طلبك:" + " " + data.title,
-      };
-      const payload = {
-        data: payloadData,
-      };
-      return await admin.messaging().sendToDevice(tokens, payload).then((response) => {
-        console.log("Pushed All Notifications");
-      });
-    }
-  })
-      .catch((err) => {
-        console.log(err);
-        console.log("failed ya deema");
-      });
+    })
+        .catch((err) => {
+          console.log(err);
+          console.log("failed ya deema");
+        });
+  }
 });
