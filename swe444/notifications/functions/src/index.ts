2508c196d8e104545ea45a0abbb1648f1f0b2e59
Deema AlMutairi <dema77.da@gmail.com>
2021-11-06 23:20:24 +0300

    notifysubscribedVolunteers function added

--- swe444/notifications/functions/src/index.ts
+++ swe444/notifications/functions/src/index.ts
@@ -23,3 +23,22 @@ export const notifyMosqueManager = functions.firestore.document("requests/{reque
   }
   console.log("Not completed yet");
 });
+
+export const notifysubscribedVolunteers = functions.firestore.document("requests/{requestID}").onCreate(async (snapshot, context) => {
+  const data = snapshot.data();
+    const token = data.token;
+    if (token.empty) {
+      console.log("No Device");
+    } else {
+      const payloadData = {
+        title: "طلب جديد",
+        message: "اصدر مسجد" + " " + data.title + "طلب جديد",
+      };
+      const payload = {
+        data: payloadData,
+      };
+      return await admin.messaging().sendToDevice(token, payload).then((response) => {
+        console.log("Pushed All Notifications");
+      });
+    }
+});
