ce63498d5077af3dfc94a5c1eb439017f92b2986
Deema AlMutairi <dema77.da@gmail.com>
2021-11-19 12:47:24 +0300

    Update index.ts

--- swe444/notifications/functions/src/index.ts
+++ swe444/notifications/functions/src/index.ts
@@ -4,21 +4,61 @@ admin.initializeApp();
 
 export const notifyMosqueManager = functions.firestore.document("requests/{requestID}").onUpdate(async (snapshot, context) => {
   const data = snapshot.after.data();
-  if (data.amount == data.donated || data.amount_requested == data.donated || data.parts_number == data.participants) {
-    const token = data.token;
-    if (token.empty) {
-      console.log("No Device");
-    } else {
-      const payloadData = {
-        title: "تم اكتمال الطلب",
-        message: "لقد تم اكتمال طلبك:" + " " + data.title,
-      };
-      const payload = {
-        data: payloadData,
-      };
-      return await admin.messaging().sendToDevice(token, payload).then((response) => {
-        console.log("Pushed All Notifications");
-      });
+  if (data.type == "تنظيم") {
+    if (data.parts_number == data.participants) {
+      const token = data.token;
+      if (token.empty) {
+        console.log("No Device");
+      } else {
+        const payloadData = {
+          title: "تم اكتمال المنظمين",
+          message: "لقد تم اكتمال طلبك:" + " " + data.title,
+        };
+        const payload = {
+          data: payloadData,
+        };
+        return await admin.messaging().sendToDevice(token, payload).then((response) => {
+          console.log("Pushed All Notifications");
+        });
+      }
+    }
+  }
+  if (data.type == "موارد") {
+    if (data.amount_requested == data.donated) {
+      const token = data.token;
+      if (token.empty) {
+        console.log("No Device");
+      } else {
+        const payloadData = {
+          title: "تم اكتمال الطلب",
+          message: "لقد تم اكتمال طلبك:" + " " + data.title,
+        };
+        const payload = {
+          data: payloadData,
+        };
+        return await admin.messaging().sendToDevice(token, payload).then((response) => {
+          console.log("Pushed All Notifications");
+        });
+      }
+    }
+  }
+  if (data.type == "مبلغ") {
+    if (data.amount == data.donated) {
+      const token = data.token;
+      if (token.empty) {
+        console.log("No Device");
+      } else {
+        const payloadData = {
+          title: "تم اكتمال المبلغ",
+          message: "لقد تم اكتمال طلبك:" + " " + data.title,
+        };
+        const payload = {
+          data: payloadData,
+        };
+        return await admin.messaging().sendToDevice(token, payload).then((response) => {
+          console.log("Pushed All Notifications");
+        });
+      }
     }
   }
   console.log("Not completed yet");
