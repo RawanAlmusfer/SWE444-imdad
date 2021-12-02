c6b163726ec7e4bf6dea6cc73bae5b1aa8b6cdc6
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2021-12-02 14:17:05 +0300

    Apply function is completed as per eventsVFeed.dart file
    i. Still need to move it [To apply.dart]

--- swe444/lib/Functions/home_screen/eventsVFeed.dart
+++ swe444/lib/Functions/home_screen/eventsVFeed.dart
@@ -374,7 +374,20 @@ class evFeed extends State<eventsv_feed> {
                       height: 30,
                       width: 70,
                       child: ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          int? wholePartsNum = document['parts_number'];
+                          int? currentPartsNum = document['participants'];
+                          String mName = document['mosque_name'].toString();
+                          String mmId = document['posted_by'].toString();
+                          String thisDocId = document.id;
+                          String title= document['title'];
+
+                          await apply(mName, mmId, wholePartsNum!,
+                              currentPartsNum!, thisDocId, title);
+
+
+
+                        },
                         child: Text(
                           "شارك",
                           textAlign: TextAlign.center,
@@ -611,68 +624,100 @@ class evFeed extends State<eventsv_feed> {
 //
 //     showAlertDialog(context, response);
 //   }
+
+  Future<void> apply(String mmName, String mmId, int wholePartsNum,
+      int currentPartsNem, String thisDocId, String title) async {
+    String vId = await FirebaseAuth.instance.currentUser!.uid;
+    String? response = '';
+    bool isExsited = false;
+    var document = await FirebaseFirestore.instance.collection('requests')
+        .doc(thisDocId)
+        .collection('applicants')
+        .doc(vId)
+        .get();
+
+    if (document.exists) {
+      if (document != null) {
+        isExsited = true;
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('محتويات هذا المتطوع فارغة')));
+      }
+    } else {
+      print('المتطوع ليس مسجل بقائمة المشاركين');
+    }
+    if (!isExsited) {
+      await FirebaseFirestore.instance
+          .collection('requests')
+          .doc(thisDocId)
+          .collection("applicants")
+          .doc(vId)
+          .set({'uid': vId})
+          .then((value) =>
+      {response = ' تم تقديم طلب للمشاركة في $title لمسجد $mmName بنجاح '})
+          .catchError((error) =>
+
+      {response = "لم يتم تفعيل التنبيهات بنجاح"});}
+
+    else response = ' لقد قمت بتقديم مُسبق  للمشاركة في $title لمسجد $mmName  ';
+    showAlertDialog(context, response);
+
+  }
 //
-//   showAlertDialog(BuildContext context, String? response) {
-//     // set up the button
-//     Widget okButton = Padding(
-//         padding: EdgeInsets.only(right: 20.w, bottom: 10.h),
-//       child:
-//     TextButton(
-//       child: Text(
-//         "موافق",
-//         textAlign: TextAlign.right,
-//         style: TextStyle(fontFamily: "Tajawal", color: Colors.white),
-//       ),
-//       style: ButtonStyle(
-//           backgroundColor:
-//               MaterialStateProperty.all<Color>(const Color(0xdeedd03c))),
-//       onPressed: () {
-//         int count = 0;
-//         Navigator.of(context).popUntil((_) => count++ >= 2);
-//       },
-//     ));
-//     // set up the AlertDialog
-//     AlertDialog alert = AlertDialog(
-//       shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.all(Radius.circular(32.0))),
-//       contentPadding: EdgeInsets.only(right: 20.w, top: 20.h, bottom: 10.h, left: 10.w),
-//       title: Text(
-//         "تأكيد عملية الاشتراك ",
-//         textAlign: TextAlign.right,
-//         style: TextStyle(
-//           fontFamily: "Tajawal",
-//           color: const Color(0xdeedd03c),
-//         ),
-//       ),
-//       content: Text(
-//         response!
-//         // feedbackResponse(response)!
-//         ,
-//         textAlign: TextAlign.right,
-//         style: TextStyle(fontFamily: "Tajawal", height: 1.5),
-//       ),
-//       actions: [
-//         okButton,
-//       ],
-//     );
-//
-//     // show the dialog
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return alert;
-//       },
-//     );
-//   }
-//
-//   String? feedbackResponse(int response) {
-//     if ((response) == 1) {
-// //Extract the mosuqe name to add in the msg
-//       return "تم تفعيل التنبيهات لمسجد \n  شاكرين لك مساهمتك";
-//     } else {
-//       return "لم يتم تفعيل التنبيهات لمسجد بنجاح";
-//     }
-//   }
+  showAlertDialog(BuildContext context, String? response) {
+    // set up the button
+    Widget okButton = Padding(
+        padding: EdgeInsets.only(right: 20.w, bottom: 10.h),
+      child:
+    TextButton(
+      child: Text(
+        "موافق",
+        textAlign: TextAlign.right,
+        style: TextStyle(fontFamily: "Tajawal", color: Colors.white),
+      ),
+      style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all<Color>(const Color(0xdeedd03c))),
+      onPressed: () {
+
+        Navigator.of(context).pop();
+      },
+    ));
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(32.0))),
+      contentPadding: EdgeInsets.only(right: 20.w, top: 20.h, bottom: 10.h, left: 10.w),
+      title: Text(
+        "تأكيد عملية التقديم ",
+        textAlign: TextAlign.right,
+        style: TextStyle(
+          fontFamily: "Tajawal",
+          color: const Color(0xdeedd03c),
+        ),
+      ),
+      content: Text(
+        response!
+        // feedbackResponse(response)!
+        ,
+        textAlign: TextAlign.right,
+        style: TextStyle(fontFamily: "Tajawal", height: 1.5),
+      ),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+
 }
 
 // Future<bool> isSubscribed(String mID) async {
