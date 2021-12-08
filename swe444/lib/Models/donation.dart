b4b067bc90a87eee395cb4b2afa5a7d8b53bc144
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2021-12-08 13:57:45 +0300

    Add some attributes in the donations collection:
    i. request_view_model.dart
    ii. donation.dart

--- swe444/lib/Models/donation.dart
+++ swe444/lib/Models/donation.dart
@@ -3,14 +3,16 @@ class Donation {
   String status;
   String donated_by;
   DateTime donation_time;
+  String uid;
 
-  Donation(this.amount, this.status, this.donated_by, this.donation_time );
+  Donation(this.amount, this.status, this.donated_by, this.donation_time, this.uid );
 
   Map<String, dynamic> toJson() => {
     'num_of_items': amount,
     'status': status,
     'donated_by': donated_by,
     'date': donation_time,
+    'uid': uid,
   };
 // formatting for upload to Firebase when creating the request
 
