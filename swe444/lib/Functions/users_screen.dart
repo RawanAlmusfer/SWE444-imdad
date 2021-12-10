26546d69afccd43ecaf94ff1e7d533dc1e737e31
Deema AlMutairi <dema77.da@gmail.com>
2021-12-10 21:22:58 +0300

    fixes

--- swe444/lib/Functions/users_screen.dart
+++ swe444/lib/Functions/users_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:swe444/Functions/decisions_tree.dart';
 import 'package:swe444/Functions/signup_login_screen.dart';
 import 'CustomPageRoute.dart';
