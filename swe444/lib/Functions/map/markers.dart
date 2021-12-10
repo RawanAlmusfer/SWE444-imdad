1f9cbb44f6d6328e57bad2d7b7fb7ae5fd4a57c4
Rawan Mohammed <53663822+acrawan@users.noreply.github.com>
2021-12-10 18:55:25 +0300

    updates on markers

--- swe444/lib/Functions/map/markers.dart
+++ swe444/lib/Functions/map/markers.dart
@@ -10,7 +10,6 @@ class MarkersViewModel {
   // Stream<QuerySnapshot<Map<String, dynamic>>>? _mosques;
   late Set<Marker> markers = {};
 
-
   Future<Set<Marker>> fetchMosques(BuildContext context) async {
     FirebaseFirestore.instance
         .collection('mosques_code')
@@ -18,17 +17,19 @@ class MarkersViewModel {
         .forEach((snapshot) {
       var i = snapshot.docs.iterator;
       while (i.moveNext()) {
-        String name= i.current['name'].toString();
-        String id= i.current.id;
+        String name = i.current['name'].toString();
+        String id = i.current.id;
 
         markers.add(Marker(
           markerId: MarkerId(i.current['name']),
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
           position: LatLng(i.current['lat'], i.current['long']),
-          infoWindow: InfoWindow(title: i.current['name'],
-          //     onTap:() {
-          //   callProfile(name,id, context );
-          // }
+          infoWindow: InfoWindow(
+            title: i.current['name'],
+                onTap:() {
+              callProfile(name,id, context );
+            }
           ),
         ));
         // print('here!!!!!!!!!!!!');
@@ -37,68 +38,79 @@ class MarkersViewModel {
     return markers;
   }
 
-  // callProfile(String name, String ID, BuildContext context) async {
-  //   bool flag = await isSubscribed(ID.toString());
-  //
-  //   var followrs = await FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(ID)
-  //       .collection("subscribedVolunteers")
-  //       .get();
-  //   String v = followrs.docs.length.toString();
-  //   String r = await countNumOfRequests(ID);
-  //   Navigator.of(context).push(CustomPageRoute(
-  //       child: MosqueMangerProfile(
-  //         isSubscribed: flag,
-  //         numOfVolunteers: v,
-  //         numOfRequests: r,
-  //         MosqueID: ID,
-  //         MosqueName: name,
-  //       )));
-  // }
-  //
-  // Future<String> countNumOfRequests(String mmId) async {
-  //   var requests =
-  //   await FirebaseFirestore.instance.collection('requests').get();
-  //   var numOfR = 0;
-  //   var requestDocs = requests.docs;
-  //
-  //   // loop over each item request
-  //   for (var doc in requestDocs) {
-  //     var requestData = doc.data();
-  //     if (requestData['posted_by'] == mmId) {
-  //       numOfR = numOfR + 1;
-  //     }
-  //   }
-  //
-  //   return numOfR.toString();
-  // }
-  //
-  // Future<bool> isSubscribed(String mID) async {
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //   var subscribedMosques = [];
-  //
-  //   var uesrDoc = await FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(user?.uid.toString())
-  //       .collection("subscribedMosqueManager")
-  //       .get();
-  //
-  //   var docs = uesrDoc.docs;
-  //   //var length = uesrDoc.docs.length;
-  //
-  //   for (var Doc in docs) {
-  //     if (!subscribedMosques.contains(Doc.id)) {
-  //       subscribedMosques.add(Doc.id);
-  //     }
-  //   }
-  //
-  //   if (subscribedMosques.contains(mID)) {
-  //     return true;
-  //   }
-  //   return false;
-  // }
+  callProfile(String name, String ID, BuildContext context) async {
+    String? id;
 
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where('mosque_name', isEqualTo: name)
+        .get()
+        .then((value) {
+      var mosque = value.docs.first;
+      id = mosque.id;
+    }).catchError((error, stackTrace) => print("error" + error.toString()));
+
+    if (id != null) {
+      var followers = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(id)
+          .collection("subscribedVolunteers")
+          .get();
+      bool flag = await isSubscribed(id.toString());
+      String v = followers.docs.length.toString();
+      String r = await countNumOfRequests(id!);
+      Navigator.of(context).push(CustomPageRoute(
+          child: MosqueMangerProfile(
+        isSubscribed: flag,
+        numOfVolunteers: v,
+        numOfRequests: r,
+        MosqueID: id!,
+        MosqueName: name,
+      )));
+    }
+  }
+
+  Future<String> countNumOfRequests(String mmId) async {
+    var requests =
+        await FirebaseFirestore.instance.collection('requests').get();
+    var numOfR = 0;
+    var requestDocs = requests.docs;
+
+    // loop over each item request
+    for (var doc in requestDocs) {
+      var requestData = doc.data();
+      if (requestData['posted_by'] == mmId) {
+        numOfR = numOfR + 1;
+      }
+    }
+
+    return numOfR.toString();
+  }
+
+  Future<bool> isSubscribed(String mID) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    var subscribedMosques = [];
+
+    var uesrDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user?.uid.toString())
+        .collection("subscribedMosqueManager")
+        .get();
+
+    var docs = uesrDoc.docs;
+    //var length = uesrDoc.docs.length;
+
+    for (var Doc in docs) {
+      if (!subscribedMosques.contains(Doc.id)) {
+        subscribedMosques.add(Doc.id);
+      }
+    }
+
+    if (subscribedMosques.contains(mID)) {
+      return true;
+    }
+    return false;
+  }
 
 // Set<Marker> get markers {
   //   return _markers;
