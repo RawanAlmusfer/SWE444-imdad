1a09d1ed12b8fc05ae1ee7266cae555b766c8bbc
Deema AlMutairi <dema77.da@gmail.com>
2021-12-10 12:08:50 +0300

    Delete after 7 days

--- swe444/lib/Functions/users_screen.dart
+++ swe444/lib/Functions/users_screen.dart
@@ -85,7 +85,7 @@ class _UsersScreenState extends State<UsersScreen> {
 
   bool isAfterOneDay(Timestamp timestamp) {
     var DonationDate = timestamp.toDate();
-    var deadline = DonationDate.add(Duration(days: 1, hours: 0, minutes: 00));
+    var deadline = DonationDate.add(Duration(days: 7, hours: 0, minutes: 00));
 
     //Testing
     // var deadline = DonationDate.add(Duration(days: 0, hours: 0, minutes: 05));
